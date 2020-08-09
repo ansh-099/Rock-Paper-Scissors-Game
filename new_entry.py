6332b2756c7c4e6759a6457e336f13cf52dad142
ansh-099 <ansh.099@gmail.com>
2020-08-09 19:26:32 +0530

    new_entry added

--- new_entry.py
+++ new_entry.py
@@ -4,19 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-cap = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 
 def get_folder(folder_name):
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
-def create_dataset(move):
+def new_entry(move):
 
     get_folder("dataset/" + str(move))
     counter = 1102
 
     while True:
-        ret, frame = cap.read()
+        ret, frame = capture.read()
         frame = cv2.flip(frame, 1)
 
         kernel = np.ones((3,3),np.uint8)
@@ -26,22 +26,27 @@ def create_dataset(move):
         
         hsv = cv2.cvtColor(r, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, np.array([2, 50, 60]), np.array([25, 150, 255]))   
+        
 
-        if cv2.waitKey(1) & 0xFF == ord('n'):
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('n'):
             path = f'dataset/{move}/{counter}.png'
             cv2.imwrite(path, mask)
 
             print(f'move no: {counter}')
             counter = counter + 1
+        elif key & 0xFF == ord('q'):
+            break
 
-        cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('frame', (800, 1200))
-        cv2.imshow('mask', mask)
-        cv2.imshow('frame', frame)
-        cv2.moveWindow("mask", 20,20);
-        cv2.moveWindow("frame", 500,0);
+        cv2.namedWindow('Image ("n" to add move, "q" to exit)', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Image ("n" to add move, "q" to exit)', (800, 1200))
+        cv2.imshow('Threshold', mask)
+        cv2.imshow('Image ("n" to add move, "q" to exit)', frame)
+        cv2.moveWindow('Threshold', 20,20);
+        cv2.moveWindow('Image ("n" to add move, "q" to exit)', 500,0);
 
 print("Create Dataset")
 move = input("Enter move name: ")
-create_dataset(move)
-
+new_entry(move)
+capture.release()
+cv2.destroyAllWindows()
